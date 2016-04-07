53899d59086a47f55a00f16e3de75a36574795e0
zaazwm <zaazwm@gmail.com>
2016-04-07 17:49:27 +0200

    added output file argument to analyser util

--- Dependency Parser/src/TransitionSequenceAnalysisUtil.java
+++ Dependency Parser/src/TransitionSequenceAnalysisUtil.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -188,8 +191,8 @@ public class TransitionSequenceAnalysisUtil {
 	}
 
 	public static void main(String[] args) {
-		if(args.length!=1) {
-			System.out.println("Usage: [dev analysis file]");
+		if(args.length!=1 && args.length!=2) {
+			System.out.println("Usage: [dev analysis file] [[output file]]");
 			return;
 		}
 		
@@ -198,6 +201,24 @@ public class TransitionSequenceAnalysisUtil {
 		if(!file.exists()) {
 			System.out.println("Usage: [dev analysis file]");
 			return;
+		}
+		
+		if(args.length==2) {
+			String outputStr = args[1];
+			File outfile = new File(outputStr);
+			if(!outfile.exists()) {
+				try {
+					outfile.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				PrintStream out = new PrintStream(new FileOutputStream(outfile, true));
+				System.setOut(out);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		try {
